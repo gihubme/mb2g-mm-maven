0f2905334772146c97bdc84d03995fc98da60bef
nikak <nikakar@gmail.com>
2021-11-04 19:34:32 +0100

    added usertest

---
+++ jpa-entities/src/test/java/guru/springframework/entities/UserTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+}
